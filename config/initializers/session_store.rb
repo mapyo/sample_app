d0554e368cf2eea148dc717af18a319110c4c789
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2015-03-18 09:13:59 +0900

    generate secret token dynamically

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Rails.application.config.session_store :cookie_store, key: secure_token
